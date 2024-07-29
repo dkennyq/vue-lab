9c6ee0653748dccce997916def24a99295310fb2
KennyQ <dev.kennyq@gmail.com>
2024-07-29 11:35:06 -0500

    Pending files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,61 +1,133 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="vote-component">
+    <div class="header">
+      <h1>{{ title }}</h1>
+      <p>{{ description }}</p>
+      <a href="#" class="more-info">More information</a>
+      <h2>What's Your Verdict?</h2>
+    </div>
+    <div class="voting">
+      <button @click="vote('up')" :class="{ active: userVote === 'up' }">
+        👍
+      </button>
+      <button @click="vote('down')" :class="{ active: userVote === 'down' }">
+        👎
+      </button>
+    </div>
+    <div class="results">
+      <div class="up-votes" :style="{ width: upVotesPercentage + '%' }">
+        {{ upVotesPercentage }}%
+      </div>
+      <div class="down-votes" :style="{ width: downVotesPercentage + '%' }">
+        {{ downVotesPercentage }}%
+      </div>
+    </div>
+    <p class="closing">CLOSING IN {{ closingDays }} days</p>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'HelloWorld',
+<script>
+export default {
+  name: "VoteComponent",
   props: {
-    msg: String,
+    title: String,
+    description: String,
+    upVotes: Number,
+    downVotes: Number,
+    closingDays: Number,
   },
-});
+  data() {
+    return {
+      userVote: null,
+    };
+  },
+  computed: {
+    totalVotes() {
+      return this.upVotes + this.downVotes;
+    },
+    upVotesPercentage() {
+      return ((this.upVotes / this.totalVotes) * 100).toFixed(1);
+    },
+    downVotesPercentage() {
+      return ((this.downVotes / this.totalVotes) * 100).toFixed(1);
+    },
+  },
+  methods: {
+    vote(type) {
+      this.userVote = type;
+      if (type === 'up') {
+        this.$emit('vote-up');
+      } else {
+        this.$emit('vote-down');
+      }
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.vote-component {
+  background: url('/path/to/your/image.jpg') no-repeat center center;
+  background-size: cover;
+  padding: 20px;
+  color: white;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.header h1 {
+  font-size: 2rem;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.header p {
+  font-size: 1rem;
 }
-a {
-  color: #42b983;
+
+.more-info {
+  color: white;
+  text-decoration: underline;
+}
+
+.voting {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+
+.voting button {
+  background: none;
+  border: 2px solid white;
+  color: white;
+  font-size: 2rem;
+  padding: 10px;
+  cursor: pointer;
+}
+
+.voting button.active {
+  background: white;
+  color: black;
+}
+
+.results {
+  display: flex;
+  margin-top: 20px;
+}
+
+.up-votes {
+  background: green;
+  color: white;
+  text-align: center;
+  padding: 10px;
+}
+
+.down-votes {
+  background: red;
+  color: white;
+  text-align: center;
+  padding: 10px;
+}
+
+.closing {
+  margin-top: 20px;
+  text-align: center;
+  font-size: 1rem;
 }
 </style>
